9d84f297c7097009e5251ae32b8343062ec6da48
mellabanca <91677889+mellabanca@users.noreply.github.com>
2021-11-29 12:17:22 -0300

    29-11

--- sketch.js
+++ sketch.js
@@ -1,28 +1,30 @@
 var dinognomo, dinognomofoto;
-
+var solopassado;
+var solopassadofoto;
 function preload(){
-  dinognomofoto = loadAnimation("trex1.png", "trex3.png", "trex4.png");
-
+dinognomofoto = loadAnimation("trex1.png", "trex3.png", "trex4.png");
+solopassadofoto = loadImage("ground2.png");
 }
-
 function setup(){
-  createCanvas(600,200);
-  dinognomo = createSprite(50,160,20,50);
-  dinognomo.addAnimation("correndo", dinognomofoto);
-  borda = createEdgeSprites();
+createCanvas(600,200);
+dinognomo = createSprite(50,160,20,50);
+dinognomo.addAnimation("correndo", dinognomofoto);
+dinognomo.scale = 0.5;
+borda = createEdgeSprites();
+solopassado = createSprite (200,180,400,20);
+solopassado.addImage("solodoido",solopassadofoto);
+solopassado.x = width/2;
 }
-
 function draw(){
-  background("white");
-
-  if(keyDown("space")){
-    dinognomo.velocityY = -10;
-  }
-
-  dinognomo.velocityY = dinognomo.velocityY + 1;
-
-  dinognomo.collide(borda[3]);
-
-  drawSprites();
-
+background("darkseagreen");
+solopassado.velocityX=-2;
+if(solopassado.x<0) {
+solopassado.x=width/2;
+}
+if(keyDown("space")){
+dinognomo.velocityY = -10;
+}
+dinognomo.velocityY = dinognomo.velocityY + 1;
+dinognomo.collide(solopassado);
+drawSprites();
 }
