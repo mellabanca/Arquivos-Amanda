f19afdd8b8419dec60fe424c0149863d879e4e23
mellabanca <91677889+mellabanca@users.noreply.github.com>
2021-12-06 14:27:35 -0300

    06/12

--- sketch.js
+++ sketch.js
@@ -2,10 +2,11 @@ var dinognomo, dinognomofoto;
 var solopassado;
 var solopassadofoto;
 var solotrapaceiro;
-
+var nuvemantiquada,nuvemantiquadaimage;
 function preload(){
     dinognomofoto = loadAnimation("trex1.png", "trex3.png", "trex4.png");
     solopassadofoto = loadImage("ground2.png");
+    nuvemantiquadaimage=loadImage("cloud.png");
 }
 
 function setup(){
@@ -19,8 +20,9 @@ createCanvas(600,200);
     solopassado.x = width/2;
     solotrapaceiro=createSprite(200,190,400,10);
     solotrapaceiro.visible=false;
-    var number = Math.round(random(1,100));
-    console.log(number);
+    //var number = Math.round(random(1,100));
+    //console.log(number);
+    console.log("Oi"+5);
 }
 
 function draw(){
@@ -40,5 +42,14 @@ drawSprites();
 }
 
 function createNuvem(){
-    
+    if(frameCount%60===0){
+        nuvemantiquada=createSprite(600,100,40,10);
+        nuvemantiquada.addImage(nuvemantiquadaimage);
+        nuvemantiquada.y=Math.round(random(10,120));
+        nuvemantiquada.velocityX=-3; 
+        nuvemantiquada.depth=dinognomo.depth;
+        dinognomo.depth=dinognomo.depth+1;
+        nuvemantiquada.lifetime = 250;
+    }
+  
 }
