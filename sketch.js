545f8091a96026ab9fedc8565edf6bbe1e75782e
mellabanca <91677889+mellabanca@users.noreply.github.com>
2021-12-08 14:18:35 -0300

    08/12

--- sketch.js
+++ sketch.js
@@ -3,10 +3,22 @@ var solopassado;
 var solopassadofoto;
 var solotrapaceiro;
 var nuvemantiquada,nuvemantiquadaimage;
+var obstaculoone,obstaculotwo,obstaculothree,obstaculofour,obstaculofive,obstaculosix;
+var nuvensbarraqueiras, obstaculosirritantes;
+var jogandofeliz = 1;
+var jogandotriste = 0;
+var estado = jogandofeliz;
+
 function preload(){
     dinognomofoto = loadAnimation("trex1.png", "trex3.png", "trex4.png");
     solopassadofoto = loadImage("ground2.png");
     nuvemantiquadaimage=loadImage("cloud.png");
+    obstaculoone=loadImage("obstacle1.png");
+    obstaculotwo=loadImage("obstacle2.png");
+    obstaculothree=loadImage("obstacle3.png");
+    obstaculofour=loadImage("obstacle4.png");
+    obstaculofive=loadImage("obstacle5.png");
+    obstaculosix=loadImage("obstacle6.png");
 }
 
 function setup(){
@@ -23,12 +35,22 @@ createCanvas(600,200);
     //var number = Math.round(random(1,100));
     //console.log(number);
     console.log("Oi"+5);
+    nuvensbarraqueiras = new Group();
+    obstaculosirritante = new Group();
 }
 
 function draw(){
-background("darkseagreen");
 //console.log(dinognomo.y);
+if(estado === jogandofeliz){
+    background("darkseagreen");
     solopassado.velocityX=-2;
+} else if(estado === jogandotriste){
+    background("darkgreen");
+    solopassado.velocityX=0;
+
+}
+
+    
 if(solopassado.x<0) {
     solopassado.x=width/2;
 }
@@ -38,6 +60,7 @@ if(keyDown("space")&&dinognomo.y>=150){
     dinognomo.velocityY = dinognomo.velocityY + 1;
     dinognomo.collide(solotrapaceiro);
 createNuvem();
+createobstaculo();
 drawSprites();
 }
 
@@ -49,7 +72,36 @@ function createNuvem(){
         nuvemantiquada.velocityX=-3; 
         nuvemantiquada.depth=dinognomo.depth;
         dinognomo.depth=dinognomo.depth+1;
+        nuvensbarraqueiras.add(nuvemantiquada);
         nuvemantiquada.lifetime = 250;
     }
   
+}
+function createobstaculo(){
+    if(frameCount%60===0){
+        var obstaculodoidao =createSprite(600,165,10,40);
+obstaculodoidao.velocityX=-6;
+var numerojurassico=Math.round(random(1,6));
+switch (numerojurassico) {
+    case 1:obstaculodoidao.addImage(obstaculoone);
+        break;
+        case 2:obstaculodoidao.addImage(obstaculotwo);
+        break;
+        case 3:obstaculodoidao.addImage(obstaculothree);
+        break;
+        case 4:obstaculodoidao.addImage(obstaculofour);
+        break;
+        case 5:obstaculodoidao.addImage(obstaculofive);
+        break;
+        case 6:obstaculodoidao.addImage(obstaculosix);
+        break;
+
+
+    default:
+        break;
+}
+obstaculodoidao.scale=0.5;
+obstaculosirritante.add(obstaculodoidao);
+obstaculodoidao.lifetime=300;
+    }
 }
