7581c0316d296527e3cb96bca52e26acaed3c724
mellabanca <91677889+mellabanca@users.noreply.github.com>
2021-12-27 14:17:20 -0300

    27/12

--- sketch.js
+++ sketch.js
@@ -37,16 +37,16 @@ function preload(){
 }
 
 function setup(){
-createCanvas(600,200);
-    dinognomo = createSprite(50,160,20,50);
+createCanvas(windowWidth,windowHeight);
+    dinognomo = createSprite(50,height-70,20,50);
     dinognomo.addAnimation("correndo", dinognomofoto);
     dinognomo.addAnimation("fracasso",dinocommedo);
     dinognomo.scale = 0.5;
     borda = createEdgeSprites();
-    solopassado = createSprite (200,180,400,20);
+    solopassado = createSprite (width/2,height-80,width,125);
     solopassado.addImage("solodoido",solopassadofoto);
     solopassado.x = width/2;
-    solotrapaceiro=createSprite(200,190,400,10);
+    solotrapaceiro=createSprite(width/2,height-10,width,125);
     solotrapaceiro.visible=false;
     //var number = Math.round(random(1,100));
     //console.log(number);
@@ -55,9 +55,9 @@ createCanvas(600,200);
     obstaculosirritantes = new Group();
     dinognomo.setCollider("circle", 0, 0, 40);
     //dinognomo.debug = true;
-    quedesgosto=createSprite(300,90);
+    quedesgosto=createSprite(width/2,height/2-50);
     quedesgosto.addImage(desgostoimage);
-    maisumachancefilho= createSprite(300,140);
+    maisumachancefilho= createSprite(width/2,height/2);
     maisumachancefilho.addImage(felicidadeimage);
 }
 
@@ -65,7 +65,7 @@ function draw(){
 //console.log(mensagemlouca); 
 //console.log(dinognomo.y);
 background("darkseagreen");
-text("Dino perto de seu pai:"+pertodeseupai,450,50);
+text("Dino perto de seu pai:"+pertodeseupai,50,50);
 //console.log("Estado do jogo:", estado);
 if(estado === jogandofeliz){
     
@@ -73,14 +73,15 @@ if(estado === jogandofeliz){
     if(solopassado.x<0) {
         solopassado.x=width/2;
     }
-    if(keyDown("space")&&dinognomo.y>=150){
+    if(touches.length > 0 || keyDown("space")&&dinognomo.y>=150){
         dinognomo.velocityY = -12;
         somsaltitante.play();
+        touches = [];
     }
         dinognomo.velocityY = dinognomo.velocityY + 1;
         createNuvem();
         createobstaculo();
-        pertodeseupai=pertodeseupai+Math.round(frameCount/60);
+        pertodeseupai=pertodeseupai+Math.round(frameRate()/60);
 
         if(pertodeseupai>0 && pertodeseupai%100===0){
             somrico.play();
@@ -90,15 +91,26 @@ if(estado === jogandofeliz){
             estado=jogandotriste;
             somtristonho.play();
         }
+        quedesgosto.visible=false;
+maisumachancefilho.visible=false;
+
 } else if(estado === jogandotriste){
+
    // background("darkgreen");
     solopassado.velocityX=0;
     dinognomo.velocityY=0;
     dinognomo.changeAnimation("fracasso",dinocommedo);
 obstaculosirritantes.setVelocityXEach(0);
 nuvensbarraqueiras.setVelocityXEach(0);
+if(mousePressedOver(maisumachancefilho)){
+    novachance();
+}
 obstaculosirritantes.setLifetimeEach(-1);
 nuvensbarraqueiras.setLifetimeEach(-1);
+quedesgosto.visible=true;
+maisumachancefilho.visible=true;
+
+
 }
 
     
@@ -106,22 +118,27 @@ nuvensbarraqueiras.setLifetimeEach(-1);
 
     dinognomo.collide(solotrapaceiro);
 
-    if(mousePressedOver(maisumachancefilho)){
-        novachance();
-    }
+    
 
 drawSprites();
 }
 
 function novachance(){
-    
+    estado=jogandofeliz;
+quedesgosto.visible=false;
+maisumachancefilho.visible=false;
+nuvensbarraqueiras.destroyEach();
+obstaculosirritantes.destroyEach();
+dinognomo.changeAnimation("correndo",dinognomofoto);
+pertodeseupai=0;
+
 }
 
 function createNuvem(){
     if(frameCount%60===0){
-        nuvemantiquada=createSprite(600,100,40,10);
+        nuvemantiquada=createSprite(width+20,height-300,40,10);
         nuvemantiquada.addImage(nuvemantiquadaimage);
-        nuvemantiquada.y=Math.round(random(10,120));
+        nuvemantiquada.y=Math.round(random(10,height/2));
         nuvemantiquada.velocityX=-3; 
         nuvemantiquada.depth=dinognomo.depth;
         dinognomo.depth=dinognomo.depth+1;
@@ -132,7 +149,7 @@ function createNuvem(){
 }
 function createobstaculo(){
     if(frameCount%60===0){
-        var obstaculodoidao =createSprite(600,165,10,40);
+        var obstaculodoidao =createSprite(width,height-95,10,40);
 obstaculodoidao.velocityX=-(6+pertodeseupai/100);
 var numerojurassico=Math.round(random(1,6));
 switch (numerojurassico) {
